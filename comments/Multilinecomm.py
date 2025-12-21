c777d1c18d20f5c09ecf7c20cb256e86c1e5d36d
upvanbhatt <upvanbhatt@gmail.com>
2025-12-21 01:13:27 -1200

    comments

---
+++ comments/Multilinecomm.py
@@ -0,0 +1,7 @@
+#this is a multiline comment
+#by using multiple hash we can write multiline comment
+''''this is multiline comment'''
+#we use ctrl +forward slash to comment out multiple lines
+# print("hello")
+# print("this is a book")
+# print ("flower is beautiful")
